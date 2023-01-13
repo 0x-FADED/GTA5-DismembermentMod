dba4c74e2362584281dd255505309ad76afc77a8
_Sakura <97910292+0x-FADED@users.noreply.github.com>
2023-01-13 19:09:55 +0900

    Create ReadME.md

--- Dismemberment/Main.cs
+++ Dismemberment/Main.cs
@@ -103,8 +103,8 @@ namespace Dismemberment
                 Ped ped2 = null;
                 if (start == 31086 || start == 39317)
                 {
-                    Function.Call(Hash.KNOCK_OFF_PED_PROP,ped, 0, 1, 1, 1);
-                    Function.Call(Hash.KNOCK_OFF_PED_PROP,ped, 1, 1, 1, 1);
+                    Function.Call(Hash.KNOCK_OFF_PED_PROP, ped, 0, 1, 1, 1);
+                    Function.Call(Hash.KNOCK_OFF_PED_PROP, ped, 1, 1, 1, 1);
                     Function.Call(Hash.STOP_PED_SPEAKING, ped, true);
                     Function.Call(Hash.DISABLE_PED_PAIN_AUDIO, ped, true);
                     if (ped.HasBeenDamagedByAnyMeleeWeapon())
@@ -119,7 +119,7 @@ namespace Dismemberment
                     }
                     else
                     {
-                        Prop prop = World.CreateProp("cap_head", ped.Position,  false, false);
+                        Prop prop = World.CreateProp("cap_head", ped.Position, false, false);
                         prop.AttachTo(ped.Bones[Bone.SkelSpine3], new Vector3(0.294f, 0.01f, -0.025f), new Vector3(0f, -90f, 160f));
                         caps.Add(prop);
                     }
@@ -138,61 +138,61 @@ namespace Dismemberment
                 }
                 else if (start == 63931)
                 {
-                    Prop prop2 = World.CreateProp("cap_calf", ped.Position,  false, false);
+                    Prop prop2 = World.CreateProp("cap_calf", ped.Position, false, false);
                     prop2.AttachTo(ped.Bones[Bone.SkelLeftThigh], new Vector3(0.25f, -0.03f, 0f), new Vector3(-40f, 90f, 0f));
                     caps.Add(prop2);
                 }
                 else if (start == 36864)
                 {
-                    Prop prop3 = World.CreateProp("cap_calf", ped.Position,  false, false);
+                    Prop prop3 = World.CreateProp("cap_calf", ped.Position, false, false);
                     prop3.AttachTo(ped.Bones[Bone.SkelRightThigh], new Vector3(0.25f, -0.03f, 0f), new Vector3(-40f, 90f, 0f));
                     caps.Add(prop3);
                 }
                 else if (start == 58271)
                 {
-                    Prop prop4 = World.CreateProp("cap_calf", ped.Position,  false, false);
+                    Prop prop4 = World.CreateProp("cap_calf", ped.Position, false, false);
                     prop4.AttachTo(ped.Bones[Bone.SkelLeftThigh], new Vector3(0.05f, 0f, 0f), new Vector3(0f, 90f, 0f));
                     caps.Add(prop4);
                 }
                 else if (start == 51826)
                 {
-                    Prop prop5 = World.CreateProp("cap_calf", ped.Position,  false, false);
+                    Prop prop5 = World.CreateProp("cap_calf", ped.Position, false, false);
                     prop5.AttachTo(ped.Bones[Bone.SkelRightThigh], new Vector3(0.05f, 0f, 0f), new Vector3(0f, 90f, 0f));
                     caps.Add(prop5);
                 }
                 else if (start == 14201)
                 {
-                    Prop prop6 = World.CreateProp("cap_upperarm", ped.Position,  false, false);
+                    Prop prop6 = World.CreateProp("cap_upperarm", ped.Position, false, false);
                     prop6.AttachTo(ped.Bones[Bone.SkelLeftCalf], new Vector3(0.3f, 0.01f, 0f), new Vector3(120f, 20f, 90f));
                     caps.Add(prop6);
                 }
                 else if (start == 52301)
                 {
-                    Prop prop7 = World.CreateProp("cap_upperarm", ped.Position,  false, false);
+                    Prop prop7 = World.CreateProp("cap_upperarm", ped.Position, false, false);
                     prop7.AttachTo(ped.Bones[Bone.SkelRightCalf], new Vector3(0.3f, 0.01f, 0f), new Vector3(120f, 20f, 90f));
                     caps.Add(prop7);
                 }
                 else if (start == 45509 || start == 64729 || start == 61163)
                 {
-                    Prop prop8 = World.CreateProp("cap_shoulder", ped.Position,  false, false);
+                    Prop prop8 = World.CreateProp("cap_shoulder", ped.Position, false, false);
                     prop8.AttachTo(ped.Bones[Bone.SkelSpine3], new Vector3(0.2f, -0.03f, 0.15f), new Vector3(-170f, -180f, -50f));
                     caps.Add(prop8);
                 }
                 else if (start == 40269 || start == 10706 || start == 28252)
                 {
-                    Prop prop9 = World.CreateProp("cap_shoulder", ped.Position,  false, false);
+                    Prop prop9 = World.CreateProp("cap_shoulder", ped.Position, false, false);
                     prop9.AttachTo(ped.Bones[Bone.SkelSpine3], new Vector3(0.18f, -0.03f, -0.15f), new Vector3(0f, 180f, 0f));
                     caps.Add(prop9);
                 }
                 else if (start == 18905)
                 {
-                    Prop prop10 = World.CreateProp("cap_shoulder", ped.Position,  false, false);
+                    Prop prop10 = World.CreateProp("cap_shoulder", ped.Position, false, false);
                     prop10.AttachTo(ped.Bones[Bone.SkelLeftUpperArm], new Vector3(0.3f, 0.02f, 0.04f), new Vector3(0f, 80f, 0f));
                     caps.Add(prop10);
                 }
                 else if (start == 57005)
                 {
-                    Prop prop11 = World.CreateProp("cap_shoulder", ped.Position,  false, false);
+                    Prop prop11 = World.CreateProp("cap_shoulder", ped.Position, false, false);
                     prop11.AttachTo(ped.Bones[Bone.SkelRightUpperArm], new Vector3(0.3f, 0.02f, 0.04f), new Vector3(0f, 80f, 0f));
                     caps.Add(prop11);
                 }
@@ -217,7 +217,7 @@ namespace Dismemberment
                 }
                 else
                 {
-                    Function.Call(Hash.STOP_CURRENT_PLAYING_SPEECH,ped);
+                    Function.Call(Hash.STOP_CURRENT_PLAYING_SPEECH, ped);
                 }
                 AddBoneDraw(ped.Handle, start, end);
                 Function.Call(Hash.USE_PARTICLE_FX_ASSET, "scr_solomon3");
---
+++ ReadME.md
@@ -0,0 +1,2 @@
+# description
+continuation of [dismemberment Mod](https://www.gta5-mods.com/scripts/dismemberment) for GTA5 which doesn't work anymore
